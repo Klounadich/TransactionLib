2c79c4a66e3a95726a309711ca89f03cd2fb63bf
Klounadich <kekers507y@gmail.com>
2026-02-17 09:01:12 +0300

    working demo

--- Transact.h
+++ Transact.h
@@ -2,24 +2,25 @@
 #define TRANSACT_H
 
 #include <algorithm>
-#include <exception>
-#include <experimental/filesystem>
+
 template<typename T>
 class Transaction {
 private:
 T* orig;
+T snapshot;
 T copy;
 bool activ;
 public:
-Transaction(T &obj) : orig(&obj) , copy(obj) , activ(true){}; //base constructor 
+Transaction(T &obj) : orig(&obj) , snapshot(obj) , activ(true){}; //base constructor 
 
 Transaction (const Transaction&) = delete; // copy lock on  initialization 
 Transaction& operator = (const Transaction&) = delete; // copy lock on created object
 
-Transaction (Transaction&& other) noexcept : orig(other.orig) , copy(std::move(other.copy) ) , activ(other.activ) {
+Transaction (Transaction&& other) noexcept : orig(other.orig) , snapshot(std::move(other.snapshot) ) , activ(other.activ) {
     other.orig = nullptr; // clear memory pointer
     other.activ = false;
-}
+} //movement constructor 
+
 
 ~Transaction(){ // destructor
     if(activ && orig) {
@@ -27,32 +28,34 @@ Transaction (Transaction&& other) noexcept : orig(other.orig) , copy(std::move(o
     }
 }
 
-T& get() {
+T& get() { //entry 
    
     return copy;
 }
 
-void commit() {
-    
+void commit() { 
+     if(activ && orig) {
+        *orig = copy; 
+    }
     activ = false;
 }
 
 void rollback()  noexcept{
     if(activ) {
         activ = false;
-        *orig=copy;
+        *orig=snapshot;
     }
     activ = false;
 }
 
-bool is_active() {
+bool is_active() { //check status
     return activ;
 }
 
 };
 
 template<typename T>
-Transaction<T> begin_transaction(T& obj) {
+Transaction<T> begin_transaction(T& obj) { // fabric func
     return Transaction<T>(obj);
 }
 
